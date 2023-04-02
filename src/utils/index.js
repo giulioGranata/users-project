1b1ec737c12aca04f1e4d7cee057e6dea85d80ca
ggiulio89 <ggiulio89@gmail.com>
2023-04-02 14:38:26 +0200

    comments

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,7 @@
+// Util function which gets an array inputArray and a size perChunk
+// and returns an array of chunks, every chunk size is maximum as
+// perChunk value.
+
 export const splitArrayIntoChunks = (inputArray, perChunk) => {
   const result = inputArray.reduce((resultArray, item, index) => {
     const chunkIndex = Math.floor(index / perChunk);
